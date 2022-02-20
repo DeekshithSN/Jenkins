a4083832d4c909d776b719e0b4aae28ba74cc1a7
Deekshith SN <deekshithsn@gmail.com>
2022-02-20 20:09:18 +0530

    Create images.sh

---
+++ scripts/images.sh
@@ -0,0 +1,8 @@
+count=$(curl -s -u admin:admin -X GET http://34.125.203.155:8081/service/rest/v1/search?repository=${1} | jq '.items[].name' | wc -l)
+
+if [ $count -eq 0 ]
+then
+        echo "no-images"
+else
+        curl -s -u admin:admin -X GET http://34.125.203.155:8081/service/rest/v1/search?repository=${1} | jq '.items[].name' | sed 's/"//g'
+fi
