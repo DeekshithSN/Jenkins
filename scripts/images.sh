02a7b035a978253c5d4ccd408e6ad02e768a5c35
Deekshith SN <deekshithsn@gmail.com>
2022-02-23 06:53:59 +0530

    Update images.sh

--- scripts/images.sh
+++ scripts/images.sh
@@ -1,8 +1,8 @@
-count=$(curl -s -u admin:admin -X GET http://34.125.203.155:8081/service/rest/v1/search?repository=${1} | jq '.items[].name' | wc -l)
-
+count=$(curl -s -u admin:admin -X GET http://34.125.203.155:8081/service/rest/v1/search?repository=${1} |
+ jq '.items[].name' | wc -l)
 if [ $count -eq 0 ]
 then
         echo "no-images"
 else
-        curl -s -u admin:admin -X GET http://34.125.203.155:8081/service/rest/v1/search?repository=${1} | jq '.items[].name' | sed 's/"//g'
+        curl -s -u admin:admin -X GET http://34.125.203.155:8081/service/rest/v1/search?repository=${1} | jq '.items[] | .name + ":" + .version' | sed 's/"//g'
 fi
